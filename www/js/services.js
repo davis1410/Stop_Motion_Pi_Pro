34f954e7053697d0cc6c758d97c020ff393b8a4e
Brennan Davis <brennan.davis1410@gmail.com>
2015-12-17 22:37:49 -0700

    brdavis: commented out debugging API requests

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,6 @@
 angular.module('stop_motion_pi_pro.services', [])
 
-.constant('DjangoCamControllerAPI', '')
+//.constant('DjangoCamControllerAPI', '')
 //.constant('DjangoCamControllerAPI', 'http://192.168.1.12:8000')
 
 .factory('connectService', function() {
@@ -26,11 +26,11 @@ angular.module('stop_motion_pi_pro.services', [])
     }
 })
 
-.factory('cameraService', function($http) {
+.factory('cameraService', function($http, DjangoCamControllerAPI) {
     return {
         clear_mount: function(ip) {
-//            var get_pid = $http.get('http://' + ip + ':8000/camera_ops/clear_mount/')
-            var get_pid = $http.get(DjangoCamControllerAPI + '/camera_ops/clear_mount/')
+            var get_pid = $http.get('http://' + ip + ':8000/camera_ops/clear_mount/')
+//            var get_pid = $http.get(DjangoCamControllerAPI + '/camera_ops/clear_mount/')
             .then(function(response) {
                 var mount_cleared = response.data.result;
                 return mount_cleared;
@@ -39,8 +39,8 @@ angular.module('stop_motion_pi_pro.services', [])
             return get_pid;
         },
         take_image: function(ip, dir) {
-//            var capture_image = $http.get('http://' + ip + ':8000/camera_ops/take_image/?image_dir=' + dir)
-            var capture_image = $http.get(DjangoCamControllerAPI + '/camera_ops/take_image/?image_dir=' + dir)
+            var capture_image = $http.get('http://' + ip + ':8000/camera_ops/take_image/?image_dir=' + dir)
+//            var capture_image = $http.get(DjangoCamControllerAPI + '/camera_ops/take_image/?image_dir=' + dir)
             .then(function(response) {
                 var image_captured = response.data.result;
                 return image_captured;
@@ -49,8 +49,8 @@ angular.module('stop_motion_pi_pro.services', [])
             return capture_image;
         },
         compile_preview: function(ip, dir, framerate) {
-//            var compile_video = $http.get('http://' + ip + ':8000/camera_ops/compile_preview/?image_dir=' + dir + '&framerate=' + framerate)
-            var compile_video = $http.get(DjangoCamControllerAPI + '/camera_ops/compile_preview/?image_dir=' + dir + '&framerate=' + framerate)
+            var compile_video = $http.get('http://' + ip + ':8000/camera_ops/compile_preview/?image_dir=' + dir + '&framerate=' + framerate)
+//            var compile_video = $http.get(DjangoCamControllerAPI + '/camera_ops/compile_preview/?image_dir=' + dir + '&framerate=' + framerate)
             .then(function(response) {
                 var movie_compiled = response.data.result;
                 return movie_compiled;
