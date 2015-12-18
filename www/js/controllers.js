ddfc557f50bb8cc8262d0592ed0cb10b0294d36c
Brennan Davis <brennan.davis1410@gmail.com>
2015-12-18 13:18:08 -0700

    brdavis: fixed take image control

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,15 +12,10 @@ angular.module('stop_motion_pi_pro.controllers', [])
 
 .controller('SetOpsCtrl', function($scope, connectService) {
     var current_ip = connectService.get_ip();
-    var current_dir = connectService.get_dir();
     var current_framerate = connectService.get_framerate();
     
     if (current_ip) {
         $scope.current_ip = current_ip;
-    };
-    
-    if (current_dir) {
-        $scope.current_dir = current_dir;
     };
     
     if (current_framerate) {
@@ -35,11 +30,6 @@ angular.module('stop_motion_pi_pro.controllers', [])
 //        $scope[form_name].$setUntouched();
     };
     
-    $scope.set_dir = function(dir) {
-        connectService.set_dir(dir);
-        $scope.current_dir = dir;
-    };
-    
     $scope.set_framerate = function(framerate) {
         connectService.set_framerate(framerate);
         $scope.current_framerate = framerate;
@@ -48,26 +38,15 @@ angular.module('stop_motion_pi_pro.controllers', [])
 
 .controller('takeImageCtrl', function($scope, $ionicPopup, connectService, cameraService) {
     var ip;
-    var dir;
     var framerate;
     
     $scope.$on('$ionicView.enter', function(e) {
         ip = connectService.get_ip();
-        dir = connectService.get_dir();
         framerate = connectService.get_framerate();
     });
     
-    $scope.clear_mount = function() {
-        cameraService.clear_mount(ip).then(function(response) {
-            var result = $ionicPopup.alert({
-                title: "Message",
-                template: response
-            })
-        })
-    };
-    
     $scope.take_image = function() {
-        cameraService.take_image(ip, dir).then(function(response) {
+        cameraService.take_image(ip).then(function(response) {
             var result = $ionicPopup.alert({
                 title: "Message",
                 template: response
@@ -76,7 +55,7 @@ angular.module('stop_motion_pi_pro.controllers', [])
     };
     
     $scope.compile_preview = function() {
-        cameraService.compile_preview(ip, dir, framerate).then(function(response) {
+        cameraService.compile_preview(ip, framerate).then(function(response) {
             var result = $ionicPopup.alert({
                 title: "Message",
                 template: response
--- www/js/services.js
+++ www/js/services.js
@@ -1,7 +1,7 @@
 angular.module('stop_motion_pi_pro.services', [])
 
-.constant('DjangoCamControllerAPI', '')
-//.constant('DjangoCamControllerAPI', 'http://192.168.1.12:8000')
+//.constant('DjangoCamControllerAPI', '')
+.constant('DjangoCamControllerAPI', 'http://localhost:8100')
 
 .factory('connectService', function() {
     return {
@@ -10,12 +10,6 @@ angular.module('stop_motion_pi_pro.services', [])
         },
         get_ip: function() {
             return localStorage['connection_ip'];
-        },
-        set_dir: function(dir) {
-            localStorage['image_directory'] = dir;
-        },
-        get_dir: function() {
-            return localStorage['image_directory'];
         },
         set_framerate: function(framerate) {
             localStorage['framerate'] = framerate;
@@ -28,19 +22,11 @@ angular.module('stop_motion_pi_pro.services', [])
 
 .factory('cameraService', function($http, DjangoCamControllerAPI) {
     return {
-        clear_mount: function(ip) {
-            var get_pid = $http.get('http://' + ip + ':8000/camera_ops/clear_mount/')
-//            var get_pid = $http.get(DjangoCamControllerAPI + '/camera_ops/clear_mount/')
-            .then(function(response) {
-                var mount_cleared = response.data.result;
-                return mount_cleared;
-            });
+        take_image: function(ip) {
+//            var url = 'http://' + ip + ':8000/camera_ops/take_image/;
+            var url = DjangoCamControllerAPI + '/camera_ops/take_image/';
             
-            return get_pid;
-        },
-        take_image: function(ip, dir) {
-            var capture_image = $http.get('http://' + ip + ':8000/camera_ops/take_image/?image_dir=' + dir)
-//            var capture_image = $http.get(DjangoCamControllerAPI + '/camera_ops/take_image/?image_dir=' + dir)
+            var capture_image = $http.get(url)
             .then(function(response) {
                 var image_captured = response.data.result;
                 return image_captured;
@@ -48,9 +34,11 @@ angular.module('stop_motion_pi_pro.services', [])
             
             return capture_image;
         },
-        compile_preview: function(ip, dir, framerate) {
-//            var compile_video = $http.get('http://' + ip + ':8000/camera_ops/compile_preview/?image_dir=' + dir + '&framerate=' + framerate)
-            var compile_video = $http.get(DjangoCamControllerAPI + '/camera_ops/compile_preview/?image_dir=' + dir + '&framerate=' + framerate)
+        compile_preview: function(ip, framerate) {
+//            var url = 'http://' + ip + ':8000/camera_ops/compile_preview/?framerate=' + framerate;
+            var url = DjangoCamControllerAPI + '/camera_ops/compile_preview/?framerate=' + framerate;
+            
+            var compile_video = $http.get(url)
             .then(function(response) {
                 var movie_compiled = response.data.result;
                 return movie_compiled;
